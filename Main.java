c88d9597e739a096dc3e8944bc1a74aa9f7bc9c8
Brian Burger <bburger@d155.org>
2019-11-19 17:25:34 +0000

    Added more comments

--- Main.java
+++ Main.java
@@ -1,27 +1,26 @@
 class Main {
   public static void main(String[] args) {
-    // Declare an array
+    // Declare an array (aways defaults to 0)
     int[] arr1 = new int[10];
-    // Decalre and initialize
+    // Decalre and initialize the array
     int[] arr2 = { 4, 5, 6, 20, 100, 42 };
-    arr1[1] = 50;
-    arr1[5] = 10;
+    arr1[1] = 50;//assign number to slot 1
+    arr1[5] = 10;//assign number to slot 5
     arr1[arr1.length - 1] = arr1[1] + arr1[5];
-    arr1[0]++;
-    arr1[2]--;
-    int sum =0;
-
-
+    arr1[0]++;//add 1 to what is stored in slot
+    arr1[2]--;//subtract 1 to what is stored in slot
+    int sum = 0;
+    //.length will return the size of the current array
     for (int i = 0; i < arr1.length; i++) {
       System.out.println(arr1[i]);
     }
     System.out.println("====ARR2========");
-    
-    for(int i=0; i<arr2.length; i++){
-      //System.out.print(arr2[i]+"\t");
-      if(arr2[i]%2==0)
-       System.out.print(arr2[i]+"\t");
-      sum +=arr2[i];
+
+    for (int i = 0; i < arr2.length; i++) {
+      // System.out.print(arr2[i]+"\t");
+      if (arr2[i] % 2 == 0)
+        System.out.print(arr2[i] + "\t");
+      sum += arr2[i];
 
     }
     System.out.println();
